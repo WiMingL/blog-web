5dc32f411257717778a3045de1cdf398d0560f29
ViMin <lwmdyx1784329@163.com>
2021-06-28 10:46:35 +0800

    test

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -8,8 +8,8 @@
         <div class="row gx-4 gx-lg-5 justify-content-center">
           <div class="col-md-10 col-lg-8 col-xl-7">
             <div class="site-heading">
-              <h1>Clean Blog</h1>
-              <span class="subheading">A Blog Theme by Start Bootstrap</span>
+              <h1>Vimin</h1>
+              <span class="subheading">文章、学习、成长、记录</span>
             </div>
           </div>
         </div>
